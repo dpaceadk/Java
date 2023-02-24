2e9a06ff01cd757b3df232c41eea0624d6e3e82d
dpaceadk <91258538+dpaceadk@users.noreply.github.com>
2023-02-24 21:01:06 +0545

    Add files via upload

---
+++ understanding scanner/number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class number {
+    public static void main(String[] abc)
+{
+    System.out.println("enter the numeber");
+    Scanner obj = new Scanner(System.in);
+    int num = obj.nextInt();
+    System.out.println("Result is:"+num +"*50="+num*50);
+    
+}
+    
+}
