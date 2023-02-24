826f54416d8d3a1cb27aae41c68dbcc0a97d65cf
dpaceadk <91258538+dpaceadk@users.noreply.github.com>
2023-02-24 21:00:22 +0545

    Add files via upload

---
+++ looping1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int money =1;
+        int i=1;
+        while ( i <= 10) {
+            money= money*i;
+            i++;
+
+        }
+        System.out.println(money);
+    }
+}
