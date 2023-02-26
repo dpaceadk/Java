5b6dea50c88a6e3b1cac47f41d2e30cbc03e129a
dpaceadk <91258538+dpaceadk@users.noreply.github.com>
2023-02-26 11:12:33 +0545

    Add files via upload

---
+++ String/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       String name="Dipesh";
+       name = name + "Adhikari";
+        System.out.println(name);
+
+
+
+        String name2 = new String( "Hello World");
+        StringBuffer name3 = new StringBuffer("Dipesh");
+        name3.append("Adhikari");
+        System.out.println(name3);
+    }
+}
