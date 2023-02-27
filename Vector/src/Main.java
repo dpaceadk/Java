a8f9f959199ac8ffd2efb0d010fbd54db0c41e17
dpaceadk <91258538+dpaceadk@users.noreply.github.com>
2023-02-27 11:36:10 +0545

    Add files via upload

---
+++ Vector/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+         int[]age = {10,20,30};
+        Vector age2 = new Vector(3,10ygtf);
+        age2.add(60);
+        age2.add(50);
+        age2.add(40);
+        age2.add('c');
+        age2.add(10);
+        age2.add(10);
+        age2.add(10);
+        age2.add(10);
+        age2.add(10);
+        age2.add(10);
+        age2.add(10);
+
+        System.out.println(age2.capacity());
+        System.out.println("Vector age capacity>>" +age2.capacity());
+
+    }
+}
