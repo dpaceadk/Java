585b4b92ebb89b132e95209eab392758f07e3fcb
dpaceadk <91258538+dpaceadk@users.noreply.github.com>
2023-02-28 11:26:31 +0545

    Add files via upload

---
+++ new proj/src/Shape.java
@@ -0,0 +1,20 @@
+public class Shape {
+    int width ;
+   int height ;
+   //default constructor
+   // Shape(){
+       // width = 10;
+       // height = 20;
+
+    //}
+
+    Shape(int w, int h){  //parameterized constructor
+        width = w;
+        height = h;
+
+    }
+    public void area(){
+       // int area = width * height;
+      // System.out.println("Area is:" +area);
+    }
+}
